cc039db75195310a61f3e6b93f43652cb021470a
Naswade21 <120067170+Naswade21@users.noreply.github.com>
2026-01-14 15:38:52 -0500

    Working on the links portion

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Chrome-Dashboard
 
+/*
+    11061 - Hunter x hunter
+    21 - One Piece
+    1535 - Death Note
+    20583 - Haikyuu!
+    38000 - Demon Slayer
+    20 - Naruto
+    1735 - Naruto Shippuden
+
+    Characters
+    Killua - 27
+    Kurapika - 28
+    Gon - 30
+    Kite - 20594
+    Franky - 64
+    Nami - 723
+    Zoro - 62
+    Leorio - 29
+
+
+/anime/{id}/pictures
+/characters/{id}/pictures
+*/
--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-/*
-    11061 - Hunter x hunter
-    21 - One Piece
-    1535 - Death Note
-    20583 - Haikyuu!
-    38000 - Demon Slayer
-    20 - Naruto
-    1735 - Naruto Shippuden
-
-    Characters
-    Killua - 27
-    Kurapika - 28
-    Gon - 30
-    Kite - 20594
-    Franky - 64
-    Nami - 723
-    Zoro - 62
-    Leorio - 29
 
 
-/anime/{id}/pictures
-/characters/{id}/pictures
-*/
+//Clock
 
+const getTime = () => {
+    const timeOfDay = new Date()
 
+    const time = document.getElementById('time')
+
+    time.textContent = timeOfDay.toLocaleTimeString("en-us", {timeStyle: "long"})
+}
+
+setInterval(getTime, 1000)
+
+//Background Image
 
 const chooseAnimorCharNumber = () => {
 
